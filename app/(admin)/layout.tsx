9cc64f7e0656df446c47981c3df40e65e71e3df4
Panha <sanhpanha62@gmail.com>
2024-04-01 00:12:23 +0700

    Test

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,18 +1,24 @@
 import SideBarComponent from "@/components/sidebar/SideBarComponent";
+import { MenuIcon } from "@/components/icon/FontAwesome";
 import "@/app/globals.css";
+import { useEffect, useRef } from 'react';
+
+
+
 export default function AdminLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html>
-			<body>
-				<aside className="fixed h-screen">
-					<SideBarComponent />
-				</aside>
-				<main className="">{children}</main>
-			</body>
-		</html>
-	);
+  return (
+    <html>
+      <body className="flex ">
+        <MenuIcon/>
+        <aside className="h-screen hidden lg:block">
+          <SideBarComponent />
+        </aside>
+        <main className="flex-1">{children}</main>
+      </body>
+    </html>
+  );
 }
--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -6,21 +6,32 @@ import Error from "./error";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import { inter, suwannaphum, localCustomFont } from "./fonts";
 
+export const MetaData = {
+  title: "ISTAD E-Commerce Web",
+  description: "We provide the best quality products for you.",
+	openGraph: {
+		title: "ISTAD E-Commerce Web",
+		description : "We provide the best quality products for you.",
+	}
+};
+
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className= {`${suwannaphum.variable} ${inter.variable} ${localCustomFont.variable} h-screen flex flex-col`}>
-				<header>
-					<NavbarComponent />
-				</header>
-				<ErrorBoundary errorComponent={Error}>
-					<Suspense fallback={<Loading />}>{children}</Suspense>
-				</ErrorBoundary>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${suwannaphum.variable} ${inter.variable} ${localCustomFont.variable} h-screen flex flex-col`}
+      >
+        <header>
+          <NavbarComponent />
+        </header>
+        <ErrorBoundary errorComponent={Error}>
+          <Suspense fallback={<Loading />}>{children}</Suspense>
+        </ErrorBoundary>
+      </body>
+    </html>
+  );
 }
