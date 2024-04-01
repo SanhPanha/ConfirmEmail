7062767731a7575eb2455cd307b7591365264a3c
Panha <sanhpanha62@gmail.com>
2024-04-01 19:49:31 +0700

    Robot.txt

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://confirm-email.vercel.app/',
+      url: 'https://confirm-email.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
